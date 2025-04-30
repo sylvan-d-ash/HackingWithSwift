f90ff5181f13fc38f88f9b951cecfbc54130b9aa
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-04-30 15:05:32 +0300

    Add new Framework, GuessTheFlag, and add required images for it

---
+++ HackingWithSwift-SwiftUISeries/GuessTheFlag/GuessTheFlagView.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagView.swift
+//  GuessTheFlag
+//
+//  Created by Sylvan  on 30/04/2025.
+//
+
+import SwiftUI
+
+public struct GuessTheFlagView: View {
+    public init() {}
+
+    public var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
