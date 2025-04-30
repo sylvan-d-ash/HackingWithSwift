9acefc94baed2a66db9ccee68f675bf939ae1b2a
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-04-30 14:02:29 +0300

    Add new target for WeSplit project

---
+++ HackingWithSwift-SwiftUISeries/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sylvan  on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
