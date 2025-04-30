5682a41e1b36ab9301fe1f75ce9163ba880ff684
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-04-30 13:28:35 +0300

    Playing around a bit

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+
+    private let students = ["Hermione", "Ron", "Harry"]
+    @State private var selectedStudent = "Harry"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Enter your name", text: $name)
+                Text("Your name is: \(name)")
+            }
+
+            Section {
+                Picker("Select your favourite student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
