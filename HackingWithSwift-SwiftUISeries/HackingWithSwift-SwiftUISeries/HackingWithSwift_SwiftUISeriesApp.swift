3a8be913c16ee792977d9daf35a98f3395279679
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-04-30 14:01:09 +0300

    Create initial main project

---
+++ HackingWithSwift-SwiftUISeries/HackingWithSwift-SwiftUISeries/HackingWithSwift_SwiftUISeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_SwiftUISeriesApp.swift
+//  HackingWithSwift-SwiftUISeries
+//
+//  Created by Sylvan  on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_SwiftUISeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
