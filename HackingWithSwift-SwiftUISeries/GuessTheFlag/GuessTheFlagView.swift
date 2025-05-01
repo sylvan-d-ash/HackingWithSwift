d5318cbaca749c454532fbc9f4bedca2132549ff
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-05-01 16:24:32 +0300

    Update the design for GuessTheFlag

--- HackingWithSwift-SwiftUISeries/GuessTheFlag/GuessTheFlagView.swift
+++ HackingWithSwift-SwiftUISeries/GuessTheFlag/GuessTheFlagView.swift
@@ -19,24 +19,54 @@ public struct GuessTheFlagView: View {
 
     public var body: some View {
         ZStack {
-            Color.blue
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
 
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of: ")
-                    Text(countries[correctAnswer])
-                }
-                .foregroundStyle(Color.white)
+            VStack {
+                Spacer()
 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number], bundle: Bundle(for: GuessTheFlagBundleLocator.self))
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of: ")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)
+
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number], bundle: Bundle(for: GuessTheFlagBundleLocator.self))
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+
+                Spacer()
+                Spacer()
+
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
