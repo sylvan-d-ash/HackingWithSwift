88ac4b1423c39096a1e743dad43ed558aa10fac6
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-05-23 14:55:18 +0300

    Moonshot: initial desing implementation

--- HackingWithSwift-SwiftUISeries/Moonshot/Mission.swift
+++ HackingWithSwift-SwiftUISeries/Moonshot/Mission.swift
@@ -17,4 +17,7 @@ struct Mission: Codable, Identifiable {
     let launchDate: String?
     let crew: [CrewRole]
     let description: String
+
+    var displayName: String { "Apollo \(id)"}
+    var image: String { "apollo\(id)" }
 }
--- HackingWithSwift-SwiftUISeries/Moonshot/MoonshotView.swift
+++ HackingWithSwift-SwiftUISeries/Moonshot/MoonshotView.swift
@@ -7,11 +7,46 @@
 
 import SwiftUI
 
+private final class MoonshotBundleLocator {}
+
 public struct MoonshotView: View {
+    private let astronauts: [String: Astronaut] = Bundle(for: MoonshotBundleLocator.self).decode("astronauts.json")
+    private let missions: [Mission] = Bundle(for: MoonshotBundleLocator.self).decode("missions.json")
+
+    private let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+
     public init() {}
 
     public var body: some View {
-        Text("Hello universe")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail View")
+                        } label: {
+                            VStack {
+                                Image(mission.image, bundle: Bundle(for: MoonshotBundleLocator.self))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
