4de56c63f3fe1e4552f842e74a717402a26c56bb
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-05-23 14:47:02 +0300

    Moonshot: Add models

---
+++ HackingWithSwift-SwiftUISeries/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Sylvan  on 23/05/2025.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ HackingWithSwift-SwiftUISeries/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Sylvan  on 23/05/2025.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
