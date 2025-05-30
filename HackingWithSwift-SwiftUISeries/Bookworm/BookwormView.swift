43fb292622a8fae8de4d8fe792db5878b50c3301
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-05-30 19:21:33 +0300

    HWS: add new framework - Bookworm

---
+++ HackingWithSwift-SwiftUISeries/Bookworm/BookwormView.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormView.swift
+//  Bookworm
+//
+//  Created by Sylvan  on 30/05/2025.
+//
+
+import SwiftUI
+
+public struct BookwormView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("Hello universe!")
+    }
+}
+
+#Preview {
+    BookwormView()
+}
