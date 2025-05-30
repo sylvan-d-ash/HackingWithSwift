645ee34b0ba1cc456527f8eff5c810556434004b
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-05-30 17:39:43 +0300

    Cupcake: add design + validation for AddressView

--- HackingWithSwift-SwiftUISeries/CupcakeCorner/AddressView.swift
+++ HackingWithSwift-SwiftUISeries/CupcakeCorner/AddressView.swift
@@ -11,10 +11,28 @@ struct AddressView: View {
     @Bindable var order: Order
 
     var body: some View {
-        Text("Hello universe!")
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink("Checkout") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    AddressView(order: Order())
+    NavigationStack {
+        AddressView(order: Order())
+    }
 }
