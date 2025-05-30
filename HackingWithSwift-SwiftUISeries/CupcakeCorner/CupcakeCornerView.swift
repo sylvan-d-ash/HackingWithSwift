a9f5fc5a1575af988303a800f5aea5b2f07b90b6
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-05-30 16:06:06 +0300

    Cupcake: playing around with AsyncImage

--- HackingWithSwift-SwiftUISeries/CupcakeCorner/CupcakeCornerView.swift
+++ HackingWithSwift-SwiftUISeries/CupcakeCorner/CupcakeCornerView.swift
@@ -11,6 +11,21 @@ public struct CupcakeCornerView: View {
     public init() {}
     
     public var body: some View {
-        Text("Hello")
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("Error loading image")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200, height: 200)
     }
+}
+
+#Preview {
+    CupcakeCornerView()
 }
