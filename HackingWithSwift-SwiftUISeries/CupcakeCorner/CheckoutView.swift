349f826e2bb93efab09db631731e96582ecf220c
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-05-30 18:04:19 +0300

    Cupcake: fleshout CheckoutView

--- HackingWithSwift-SwiftUISeries/CupcakeCorner/CheckoutView.swift
+++ HackingWithSwift-SwiftUISeries/CupcakeCorner/CheckoutView.swift
@@ -11,10 +11,36 @@ struct CheckoutView: View {
     @Bindable var order: Order
 
     var body: some View {
-        Text("Hello universe!")
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else if phase.error != nil {
+                        Text("There was an error loading the image: \(phase.error!.localizedDescription)")
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(height: 233)
+
+                Text("Your total is \(order.cost, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
+                    .font(.title)
+
+                Button("Place Order", action: {})
+                    .padding()
+            }
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
 #Preview {
-    CheckoutView(order: Order())
+    NavigationStack {
+        CheckoutView(order: Order())
+    }
 }
