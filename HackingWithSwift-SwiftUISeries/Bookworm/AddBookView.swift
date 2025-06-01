b64a165bdba4071618a3966dd097b142a3257abb
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-01 15:10:27 +0300

    Bookworm: functionality to add a new book

---
+++ HackingWithSwift-SwiftUISeries/Bookworm/AddBookView.swift
@@ -0,0 +1,60 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Sylvan  on 01/06/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+
+    @State private var title: String = ""
+    @State private var author: String = ""
+    @State private var genre: String = ""
+    @State private var rating: Int = 1
+    @State private var review: String = ""
+
+    private let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Name of Book", text: $title)
+                    TextField("Author's name", text: $author)
+
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+
+                Section("Write a review") {
+                    TextEditor(text: $review)
+
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                }
+
+                Section {
+                    Button("Save") {
+                        let book = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(book)
+                    }
+                }
+            }
+            .navigationTitle("Add New Book")
+        }
+    }
+}
+
+#Preview {
+    AddBookView()
+}
