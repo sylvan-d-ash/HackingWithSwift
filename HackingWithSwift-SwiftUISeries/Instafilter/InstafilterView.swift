2bc305e40476b44b92f705aac2852b72505bd500
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-02 20:02:21 +0300

    Instafilter: initial design

--- HackingWithSwift-SwiftUISeries/Instafilter/InstafilterView.swift
+++ HackingWithSwift-SwiftUISeries/Instafilter/InstafilterView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 public struct InstafilterView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity: Double = 0.5
+
     public init() {}
 
     public var body: some View {
-        Text("Hello universe")
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity, in: 0...1)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+
+                    Spacer()
+
+                    // share pic
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+
+    private func changeFilter() {
+        //
     }
 }
