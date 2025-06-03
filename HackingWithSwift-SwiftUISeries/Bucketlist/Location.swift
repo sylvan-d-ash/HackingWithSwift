2ef35e5cb8326828242793d7820cb061dae971a7
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-03 17:33:49 +0300

    Bucketlist: switch from Marker to Annotation + clean up model a bit

--- HackingWithSwift-SwiftUISeries/Bucketlist/Location.swift
+++ HackingWithSwift-SwiftUISeries/Bucketlist/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import MapKit
 
 struct Location: Identifiable, Equatable, Codable {
     let id: UUID
@@ -13,4 +14,18 @@ struct Location: Identifiable, Equatable, Codable {
     var description: String
     var latitude: Double
     var longitude: Double
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
+
+extension Location {
+    #if DEBUG
+    static let example = Location(id: UUID(), name: "Mombasa", description: "Awesome coastal city", latitude: -4.1, longitude: 39.7)
+    #endif
+
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
