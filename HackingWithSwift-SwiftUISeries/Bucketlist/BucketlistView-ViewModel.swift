0d2253443d49ffff3f940f529a550c4f82aa7028
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-03 19:35:08 +0300

    Bucketlist: save and load Locations to file

--- HackingWithSwift-SwiftUISeries/Bucketlist/BucketlistView-ViewModel.swift
+++ HackingWithSwift-SwiftUISeries/Bucketlist/BucketlistView-ViewModel.swift
@@ -11,12 +11,24 @@ import CoreLocation
 extension BucketlistView {
     @Observable
     final class ViewModel {
-        private(set) var locations = [Location]()
+        private let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+
+        private(set) var locations: [Location]
         var selectedPlace: Location?
+
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
 
         func addLocation(at coordinate: CLLocationCoordinate2D) {
             let location = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
             locations.append(location)
+            save()
 
             selectedPlace = location
         }
@@ -26,6 +38,16 @@ extension BucketlistView {
 
             guard let index = locations.firstIndex(of: selectedPlace) else { return }
             locations[index] = location
+            save()
+        }
+
+        private func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data")
+            }
         }
     }
 }
