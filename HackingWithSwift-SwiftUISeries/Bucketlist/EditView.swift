c8e9730f77d703f2b81bc76c397cd8fc27abcb0b
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-03 17:46:45 +0300

    Bucketlist: open EditView when annotation is long pressed

---
+++ HackingWithSwift-SwiftUISeries/Bucketlist/EditView.swift
@@ -0,0 +1,16 @@
+//
+//  EditView.swift
+//  Bucketlist
+//
+//  Created by Sylvan  on 03/06/2025.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var location: Location
+
+    var body: some View {
+        Text("Hello universe")
+    }
+}
