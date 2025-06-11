fdf680f21a7b8df6809dfa98d1eed0b2bda90ddc
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-11 18:09:49 +0300

    hot-prospects: add new framework - HotProspects

---
+++ HackingWithSwift-SwiftUISeries/HotProspects/HotProspectsView.swift
@@ -0,0 +1,16 @@
+//
+//  HotProspectsView.swift
+//  HotProspects
+//
+//  Created by Sylvan  on 11/06/2025.
+//
+
+import SwiftUI
+
+public struct HotProspectsView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("hello universe!")
+    }
+}
