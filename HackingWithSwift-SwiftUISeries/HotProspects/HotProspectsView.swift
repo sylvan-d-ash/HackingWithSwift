066ab613d547b4b40052154e8507bf38ca73f8a8
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-13 11:30:08 +0300

    hot-prospects: placeholder code + files

--- HackingWithSwift-SwiftUISeries/HotProspects/HotProspectsView.swift
+++ HackingWithSwift-SwiftUISeries/HotProspects/HotProspectsView.swift
@@ -11,12 +11,23 @@ public struct HotProspectsView: View {
     public init() {}
 
     public var body: some View {
-        NavigationStack {
-            List {
-                NavigationLink("Local Notification") {
-                    LocalNotificationsView()
+        TabView {
+            ProspectsView()
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
                 }
-            }
+            ProspectsView()
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView()
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
         }
     }
 }
