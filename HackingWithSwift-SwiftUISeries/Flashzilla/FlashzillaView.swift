ad4a5ac6937ac2d139999331fb25cdb9ee4ad3aa
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-16 18:24:51 +0300

    hws: start new framework project 'Flashzilla'

---
+++ HackingWithSwift-SwiftUISeries/Flashzilla/FlashzillaView.swift
@@ -0,0 +1,20 @@
+//
+//  FlashzillaView.swift
+//  Flashzilla
+//
+//  Created by Sylvan  on 16/06/2025.
+//
+
+import SwiftUI
+
+public struct FlashzillaView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("Hello universe")
+    }
+}
+
+#Preview {
+    FlashzillaView()
+}
