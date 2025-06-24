5ffa57e0b7e66a9f8b14d8d15136de5a575d3349
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-24 13:25:08 +0300

    flashzilla: initial design of card view

--- HackingWithSwift-SwiftUISeries/Flashzilla/CardView.swift
+++ HackingWithSwift-SwiftUISeries/Flashzilla/CardView.swift
@@ -5,4 +5,35 @@
 //  Created by Sylvan  on 24/06/2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .shadow(radius: 10)
+
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+
+                Text(card.answer)
+                    .font(.title)
+                    .foregroundStyle(.secondary)
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        // TIP: the smallest iPhones have a landscape width of 480 points,
+        // so this means our card will be fully visible on all devices
+        .frame(width: 450, height: 250)
+    }
+}
+
+#Preview {
+    CardView(card: Card.example)
+}
