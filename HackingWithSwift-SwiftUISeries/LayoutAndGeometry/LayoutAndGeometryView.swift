62a8c8b8de64d0b2b4b14533d5936516c707ceeb
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-06-24 15:48:39 +0300

    layout-geometry: add new framework LayoutAndGeometry

---
+++ HackingWithSwift-SwiftUISeries/LayoutAndGeometry/LayoutAndGeometryView.swift
@@ -0,0 +1,20 @@
+//
+//  LayoutAndGeometryView.swift
+//  LayoutAndGeometry
+//
+//  Created by Sylvan  on 24/06/2025.
+//
+
+import SwiftUI
+
+public struct LayoutAndGeometryView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("Hello universe!")
+    }
+}
+
+#Preview {
+    LayoutAndGeometryView()
+}
