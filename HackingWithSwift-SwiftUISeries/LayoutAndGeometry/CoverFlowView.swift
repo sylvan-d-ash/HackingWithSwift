b61b41c36738930c8ddef376da7ae70a5493c26f
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-07-01 16:59:19 +0300

    layout-geometry: using visualEffects modifier to achieve the same CoverFlow style

--- HackingWithSwift-SwiftUISeries/LayoutAndGeometry/CoverFlowView.swift
+++ HackingWithSwift-SwiftUISeries/LayoutAndGeometry/CoverFlowView.swift
@@ -28,6 +28,35 @@ struct CoverFlowView: View {
     }
 }
 
+
+struct CoverFlowVisualEffectsView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    Text("Number \(num)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.blue)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                            content
+                                .rotation3DEffect(
+                                    .degrees(-proxy.frame(in: .global).midX) / 8,
+                                    axis: (x: 0, y: 1, z: 0))
+                        }
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
+    }
+}
+
 #Preview {
     CoverFlowView()
+}
+
+#Preview {
+    CoverFlowVisualEffectsView()
 }
