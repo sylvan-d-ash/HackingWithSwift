80511091120c4497e5a2a188d3b187d5f6fe1479
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-07-14 12:39:24 +0300

    snowseeker: add a Welcome view

---
+++ HackingWithSwift-SwiftUISeries/SnowSeeker/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Sylvan  on 14/07/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker!")
+                .font(.largeTitle)
+
+            Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+                .foregroundStyle(.secondary)
+        }
+    }
+}
